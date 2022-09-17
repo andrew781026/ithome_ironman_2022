d4eee279077f3954d725bcbc80ec6a5b28dfa7d3
andrew <andrew7810262000@gmail.com>
2022-09-17 20:58:19 +0800

    [puppeteer] 追加註解

--- puppeteer/write-article.js
+++ puppeteer/write-article.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-require('dotenv').config({ path: '.env-other' });
+require('dotenv').config({ path: '.env' });
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -13,18 +13,28 @@ require('dotenv').config({ path: '.env-other' });
     await page.click('button[type="submit"]');
 
     await page.goto('https://ithelp.ithome.com.tw/');
+
+    // 點擊 "登入/註冊" 按鈕
     await page.click('ul.menu__right a.menu__item-link');
     await page.waitForTimeout(4000);
+
+    // 點擊 "鐵人發文" 按鈕
     await page.click('button[data-target="#ir-select-series__common"]');
     await page.waitForTimeout(4000);
+
+    // 點擊 "主題區域"
     await page.click('.ir-modal__list-link');
     await page.waitForTimeout(4000);
+
+    // 填寫 "標題 & 內文"
     await page.type('input[name="subject"]', process.env.TITLE);
     await page.type('div.CodeMirror.cm-s-paper.CodeMirror-wrap textarea', fs.readFileSync(process.env.CONTENT_PATH, 'utf8').toString());
     await page.waitForTimeout(4000);
+
+    // 點擊 "發文按鈕"
     await page.click('button.save-group__dropdown-toggle');
     await page.waitForTimeout(4000);
     await page.click('input.save-group__dropdown-btn--publish[type="submit"]');
 
-    // await browser.close();
+    await browser.close();
 })();
